aea1016df85f13006221231f735d1f901feff49f
Zac <zgibson@live.com>
2023-04-13 22:49:44 -0500

    Build: Add CI and stuff (#1)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MacAudioWizard",
     products: [
-        .executable(name: "macaudiowizard", targets: ["MacAudioWizard"])
+        .executable(name: "MacAudioWizard", targets: ["MacAudioWizard"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
